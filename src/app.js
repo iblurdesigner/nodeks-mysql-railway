7cd493057f8e43cb7729d3ecc23d6391b242f60a
iblurdesigner <nevomen@gmail.com>
2024-01-30 18:16:30 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import expres from "express";
+import { pool } from "./db.js";
+import { PORT } from "./config.js";
+
+const app = expres();
+
+app.get("/", async (req, res) => {
+  const [rows] = await pool.query("SELECT * FROM users");
+  res.json(rows);
+})
+
+app.get("/ping", async (req, res) => {
+  const [result] = await pool.query(`SELECT "hello world" as RESULT`);
+  res.json(result[0])
+})
+
+app.get('/create', async (req, res) => {
+  const result = await pool.query('INSERT INTO users (name) VALUES ("David")')
+  res.json(result)
+})
+
+app.listen(PORT, () => {
+  console.log(`server on port ${PORT}`)
+})
